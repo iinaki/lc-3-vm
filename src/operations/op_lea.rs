a1dbb590164e29b5bdbcd2aebb4f6c81a38fe14b
iñaki llorens <inakillorens@Laptop-de-inaki.local>
2024-09-02 11:18:01 -0300

    Add tests for op_lea

--- src/operations/op_lea.rs
+++ src/operations/op_lea.rs
@@ -11,7 +11,67 @@ use super::{sign_extend, update_flags};
 
 pub fn op_lea(register: &mut Register, instr: u16) {
     let r0 = (instr >> 9) & 0x7;
-    let pc_offset = sign_extend(instr & 0x1FF, 9);
-    register.set(r0, register.pc + pc_offset);
+    let pc_offset = sign_extend(instr & 0x1FF, 9) as i16;
+    register.set(r0, (register.pc as i16 + pc_offset) as u16);
     update_flags(register, r0);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::constants::FL_ZRO;
+
+    use super::*;
+
+    #[test]
+    fn test_op_lea_positive_offset() {
+        let mut register = Register::new();
+
+        let instr: u16 = 0b1110_000_000000101; // LEA R0, PC + 5
+        op_lea(&mut register, instr);
+
+        assert_eq!(register.get(0), 0x3005);
+    }
+
+    #[test]
+    fn test_op_lea_negative_offset() {
+        let mut register = Register::new();
+
+        let instr: u16 = 0b1110_000_111111011; // LEA R0, PC - 5
+        op_lea(&mut register, instr);
+
+        assert_eq!(register.get(0), 0x2FFB); 
+    }
+
+    #[test]
+    fn test_op_lea_zero_offset() {
+        let mut register = Register::new();
+
+        let instr: u16 = 0b1110_000_000000000; // LEA R0, PC + 0
+        op_lea(&mut register, instr);
+
+        assert_eq!(register.get(0), 0x3000);
+    }
+
+    #[test]
+    fn test_op_lea_update_flags() {
+        let mut register = Register::new();
+        register.pc = 0x0000;
+
+        let instr: u16 = 0b1110_000_000000000; // LEA R0, PC + 0
+        op_lea(&mut register, instr);
+
+        assert_eq!(register.get(0), 0x0000);
+        assert_eq!(register.cond, FL_ZRO);
+    }
+
+    #[test]
+    fn test_op_lea_preserves_pc() {
+        let mut register = Register::new();
+
+        let instr: u16 = 0b1110_000_000000101; // LEA R0, PC + 5
+        let initial_pc = register.pc; 
+        op_lea(&mut register, instr);
+
+        assert_eq!(register.pc, initial_pc); 
+    }
 }
