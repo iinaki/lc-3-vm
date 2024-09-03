54c9bd0eddba2f25afc11880c23f0c87631f7743
iinaki <llorensinaki22@gmail.com>
2024-09-03 17:20:27 -0300

    Add doc to memory struct

--- src/memory.rs
+++ src/memory.rs
@@ -5,6 +5,11 @@ use crate::{
     utils::flush_stdout,
 };
 
+/// Represents the memory of the LC-3 virtual machine.
+/// 
+/// Contains an array representing the memory of the virtual machine,
+/// allowing for reading and writing operations at specific memory addresses.
+/// 
 #[derive(Debug)]
 pub struct Memory {
     pub memory: [u16; MEMORY_SIZE],
@@ -17,12 +22,35 @@ impl Default for Memory {
 }
 
 impl Memory {
+    /// Creates a new `Memory` instance.
+    ///
+    /// # Returns
+    ///
+    /// A `Memory` instance with all memory cells set to `0`.
+    ///
     pub fn new() -> Memory {
         Memory {
             memory: [0; MEMORY_SIZE],
         }
     }
 
+
+    /// Reads the value stored at the specified memory address.
+    ///
+    /// If the address corresponds to the keyboard status register (`MR_KBSR`), this method
+    /// checks for input from the standard input. If a character is available, it updates
+    /// the keyboard status register and the keyboard data register (`MR_KBDR`) accordingly.
+    ///
+    /// # Arguments
+    ///
+    /// * `address` - A `u16` value representing the memory address to read from.
+    ///
+    /// # Returns
+    ///
+    /// The value stored at the specified memory address. If the address corresponds to
+    /// `MR_KBSR` and an error occurs while reading from standard input, the function
+    /// prints an error message and returns `0`.
+    /// 
     pub fn read(&mut self, address: u16) -> u16 {
         if address == MR_KBSR {
             let mut buffer = [0; 1];
@@ -44,6 +72,13 @@ impl Memory {
         self.memory[address as usize]
     }
 
+    /// Writes a value to the specified memory address.
+    ///
+    /// # Arguments
+    ///
+    /// * `address` - A `u16` value representing the memory address to write to.
+    /// * `val` - The value to store at the specified memory address.
+    /// 
     pub fn write(&mut self, address: u16, val: u16) {
         self.memory[address as usize] = val;
     }
